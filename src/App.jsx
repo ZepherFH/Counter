6fd7d0624be95ec74a98c2c5f82f92a66e8f5f84
Marc GMO <marc.gmo17@gmail.com>
2023-05-14 20:11:10 -0600

    Counter

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function App() {
+  //let counter = 0;
+  const [counter, setCounter] = useState(0) //Definir una variable de estado
+
+  return (
+    <div className="blockCounter">
+      <h3>Counter</h3>
+      <hr />
+      <h1>{counter}</h1>
+        
+        <button 
+          onClick={() => {
+           setCounter(counter + 1);
+            console.log(counter)
+          }}
+          type="button" 
+          className='btn btn-primary'
+          >+1</button>
+
+          <button 
+          onClick={() => {
+           setCounter(counter - 1);
+            console.log(counter)
+          }}
+          type="button" 
+          className='btn btn-danger'
+          >-1</button>
+
+          <button 
+          onClick={() => {
+           setCounter(0);
+            console.log(counter)
+          }}
+          type="button" 
+          className='btn btn-warning'
+          > 0</button>
+
+    </div>
+  )
+}
+export default App
